a92abd81f8561580e7afd3ee3d01b8cdd3b6e65f
Tharindu37 <tharindut520@gmail.com>
2024-12-06 01:53:49 +0530

    jwt auth converter

--- src/main/java/com/ecom/user_service_api/config/JwtAuthConverter.java
+++ src/main/java/com/ecom/user_service_api/config/JwtAuthConverter.java
@@ -1,4 +1,44 @@
 package com.ecom.user_service_api.config;
 
-public class JwtAuthConverter {
+
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+
+@Component
+public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+    @Override
+    public AbstractAuthenticationToken convert(Jwt source) {
+        Collection<GrantedAuthority> grantedAuthorities = extractAuthorities(source);
+        return new JwtAuthenticationToken(source, grantedAuthorities);
+    }
+
+    private Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
+        if (jwt.getClaim("realm_access")!=null){
+            Map<String, Object> realmAccess=jwt.getClaim("realm_access");
+            ObjectMapper mapper = new ObjectMapper();
+            List<String> keyCloakRoles = mapper.convertValue(realmAccess.get("roles"), new TypeReference<List<String>>() {
+
+            });
+            List<GrantedAuthority> roles = new ArrayList<>();
+            for (String role : keyCloakRoles) {
+                roles.add(new SimpleGrantedAuthority(role));
+            }
+            return roles;
+        }
+        return new ArrayList<>();
+    }
 }
